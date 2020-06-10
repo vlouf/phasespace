2aa0f3c2be4b7a9579fabac0d4c9a6c6048e9917
Valentin Louf <valentin.louf@bom.gov.au>
2020-06-10 16:44:25 +1000

    FIX: module

--- phasespace/phase.py
+++ phasespace/phase.py
@@ -2,9 +2,9 @@
 Histogram (density, averaging and variance) in a phase-space diagram.
 
 @author: Valentin Louf
-@corporation: Monash University
-@date: 07/09/2019
-@email: <valentin.louf@monash.edu>
+@corporation: Monash University and the Australian Bureau of Meteorology
+@date: 10/06/2019
+@email: <valentin.louf@bom.gov.au>
 """
 import numpy as np
 from .phasespace import phase_space_avg, phase_space_var, phase_space_count
@@ -66,17 +66,10 @@ def phasespace(x, y, z=None, *, bins=10, range=None, kind='mean'):
         yout = np.linspace(range[1][0], range[1][1], N1)
 
     if z is None:
-<<<<<<< HEAD:phasespace/phase.py
-        phase = phase_space_count(x, y, xout, yout)    
+        phase = phase_space_count(x, y, xout, yout)
     else:
         if kind == 'mean':
-            phase = phase_space_avg(x, y, z, xout, yout)    
-=======
-        phase = phasespace.phase_space_count(x, y, xout, yout)
-    else:
-        if kind == 'mean':
-            phase = phasespace.phase_space_avg(x, y, z, xout, yout)
->>>>>>> f640c1cf8c9a28d215b1b250b9dd6388663689cc:phase/phase.py
+            phase = phase_space_avg(x, y, z, xout, yout)
         elif kind == 'var':
             phase = np.ma.masked_equal(phase_space_var(x, y, z, xout, yout), -9999)
     return  phase
